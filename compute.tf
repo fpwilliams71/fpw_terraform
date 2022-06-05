550becb0e03036017cd2e65a435cf53105fef04c
fpwilliams71 <fpwilliams71@yahoo.com>
2022-06-05 18:16:54 +0100

    Add application files

--- compute.tf
+++ compute.tf
@@ -27,39 +27,93 @@ resource "azurerm_storage_account" "stor" {
   account_replication_type = "LRS"
 }
 
-resource "azurerm_virtual_machine" "vm" {
+resource "azurerm_windows_virtual_machine" "vm" {
+  resource "azurerm_network_interface" "nic" {
+  name                = "book-nic"
+  location            = "West Europe"
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "bookipconfig"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                = var.ip_name
+  location            = "West Europe"
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+  domain_name_label   = "bookdevops"
+}
+
+resource "azurerm_storage_account" "stor" {
+  name                     = "bookstor"
+  location                 = "West Europe"
+  resource_group_name      = azurerm_resource_group.rg.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_windows_virtual_machine" "vm" {
   name                  = "bookvm"
   location              = "West Europe"
   resource_group_name   = azurerm_resource_group.rg.name
-  vm_size               = "Standard_DS1_v2"
+  size               = "Standard_DS1_v2"
   network_interface_ids = [azurerm_network_interface.nic.id]
+  admin_password = var.password
+  admin_username = var.username
 
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
+  os_disk {
+  caching              = "ReadWrite"
+  storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
     version   = "latest"
   }
 
-  storage_os_disk {
-    name              = "book-osdisk"
-    managed_disk_type = "Standard_LRS"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
+  winrm_listener {
+    protocol = "Http"
+  }
+  identity {
+    type = "SystemAssigned"
   }
 
-  os_profile {
-    computer_name  = "VMBOOK"
-    admin_username = "admin"
-    admin_password = "book123*"
+  custom_data = filebase64("./files/init-script.ps1")
+
+  additional_unattend_content {
+    setting = "AutoLogon"
+    content = "<AutoLogon><Password><Value>${var.password}</Value></Password><Enabled>true</Enabled><LogonCount>1</LogonCount><Username>${var.username}</Username></AutoLogon>"
   }
 
-  os_profile_linux_config {
-    disable_password_authentication = false
+  additional_unattend_content {
+    setting = "FirstLogonCommands"
+    content = file("./files/FirstLogonCommands.xml")
+  }
+  connection {
+    host     = azurerm_public_ip.pip.fqdn
+    type     = "winrm"
+    port     = 5985
+    https    = false
+    insecure = true
+    timeout  = "5m"
+    user     = var.username
+    password = var.password
+  }
+  provisioner "file" {
+    source      = "files"
+    destination = "c:/terraform"
   }
 
   boot_diagnostics {
     enabled     = true
-    storage_uri = azurerm_storage_account.stor.primary_blob_endpoint
+    storage_account_uri  = azurerm_storage_account.stor.primary_blob_endpoint
   }
 }
+
--- variables.tf
+++ variables.tf
@@ -1,5 +1,6 @@
 variable "resoure_group_name" {
   description = "Name of the resource group"
+  default = "fp_williams_RG"
 }
 
 variable "location" {
@@ -9,8 +10,12 @@ variable "location" {
 
 variable "application_name" {
   description = "Name of the application"
+  default = "helloword_app"
 }
 
 variable "ip_name" {
   description = "Name of the public ip"
+  default = "fp_williams_IP"
 }
+variable "password" {}
+variable "username" {}
